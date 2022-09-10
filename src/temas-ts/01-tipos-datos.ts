6f7e4360e0227f7bc297fbd41c710dfd3066b9e3
misagayta <misagayta@gmail.com>
2022-09-10 12:07:04 -0500

    Sbado 10-09-22

---
+++ src/temas-ts/01-tipos-datos.ts
@@ -0,0 +1,17 @@
+
+
+console.log('Hola Mundo!- 1002');
+
+console.log("hola mundo");
+
+var t1:number | string=3;
+let t2:number | string=2;
+const t3=444;
+let activo=boolean=false;
+
+
+t1 = 'www';
+t2 = 'www';
+
+console.log(t1);
+console.log(t2);
